848d15eb45101c745f76f91ed41b786131c10839
Will R <williamp.robertson@gmail.com>
2019-02-05 18:47:17 -0700

    added get route for albums

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,25 +1,29 @@
 //   load in routes to our data to power the app
 
 var friends = require("../data/friends.js");
+const albums = require("../data/albums.js");
 
 // const friendsList = JSON.parse(JSON.stringify(friends));
 // console.log(friendsList.friends);
 // friendsList = friendsList.friends;
 
-
 //-----------Routes
 
-module.exports = function (app) {
-
-
+module.exports = function(app) {
   //   show all match potentials - API GET Request
-  app.get("/api/friends", function (req, res) {
+  app.get("/api/friends", function(req, res) {
     // this will load when user first hits page
     res.json(friends);
   });
 
+  //   show all match potentials - API GET Request
+  app.get("/api/albums", function(req, res) {
+    // this will load when user first hits page
+    res.json(albums);
+  });
+
   //    save answers on submit and return closest match - API POST Request
-  app.post("/api/friends", function (req, res) {
+  app.post("/api/friends", function(req, res) {
     //    in this section our server will take the user survey results then
     //    compare with our database to return the best 'match' by calculating
     //    an overall difference rating numbers. Then the user will become part of database
@@ -53,7 +57,9 @@ module.exports = function (app) {
         var currentUserScore = userScores[j];
 
         // calculate difference between scores and add into totalDifference
-        totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+        totalDifference += Math.abs(
+          parseInt(currentUserScore) - parseInt(currentFriendScore)
+        );
       }
 
       // If the total of difference is less than differences of current 'best match'
@@ -62,7 +68,6 @@ module.exports = function (app) {
         bestMatch.name = currentFriend.name;
         bestMatch.photo = currentFriend.photo;
         bestMatch.friendDifference = totalDifference;
-
       }
     }
     // Save user's data to database after the check to ensure its not comparing to self
