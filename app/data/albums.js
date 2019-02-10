f8c41a9be032edf8c12eab20a176a7736d3ad9d8
Will R <williamp.robertson@gmail.com>
2019-02-10 11:34:04 -0700

    album api updates

--- app/data/albums.js
+++ app/data/albums.js
@@ -1,7 +1,7 @@
 var albums = [
   {
     title: "SELF",
-    artist: "Samuelthe1st",
+    artist: "SamuelThe1st",
     url: "https://itunes.apple.com/us/album/self/1446538476",
     image: "https://m.media-amazon.com/images/I/711xF8AIF9L._SS500_.jpg",
     thumbnail_image:
@@ -9,7 +9,7 @@ var albums = [
   },
   {
     title: "Time's Up - Single",
-    artist: "Samuelthe1st",
+    artist: "SamuelThe1st",
     url: "https://itunes.apple.com/us/album/times-up/1450002682?i=1450002685",
     image: "https://m.media-amazon.com/images/I/71FruGreD5L._SS500_.jpg",
     thumbnail_image:
@@ -17,7 +17,7 @@ var albums = [
   },
   {
     title: "Yuh - EP",
-    artist: "Samuelthe1st",
+    artist: "SamuelThe1st",
     url: "https://itunes.apple.com/us/album/yuh-ep/1293513201",
     image: "https://m.media-amazon.com/images/I/71N6-aRpGaL._SS500_.jpg",
     thumbnail_image:
@@ -25,7 +25,7 @@ var albums = [
   },
   {
     title: "Bo Jackson - Single",
-    artist: "Samuelthe1st",
+    artist: "SamuelThe1st",
     url: "https://itunes.apple.com/us/album/bo-jackson-single/1201539218",
     image: "https://m.media-amazon.com/images/I/91ejgLZN-RL._SS500_.jpg",
     thumbnail_image:
@@ -33,15 +33,15 @@ var albums = [
   },
   {
     title: "Green Room - Single",
-    artist: "Samuelthe1st",
-    url: "https://itunes.apple.com/us/album/green-room-single/1201464122",
+    artist: "SamuelThe1st",
+    url: "https://itunes.apple.com/us/album/green-room/1201464122?i=1201465071",
     image: "https://m.media-amazon.com/images/I/A1TcVqx0v5L._SS500_.jpg",
     thumbnail_image:
       "https://instagram.fphx1-1.fna.fbcdn.net/vp/a30c15ffacdaeb94f8edb49e1a6b96a2/5CE84841/t51.2885-19/s150x150/50602409_1259502774227622_243420801963917312_n.jpg?_nc_ht=instagram.fphx1-1.fna.fbcdn.net"
   },
   {
     title: "Limelit (feat. Lang)",
-    artist: "Samuelthe1st",
+    artist: "SamuelThe1st",
     url:
       "https://itunes.apple.com/us/album/limelit-feat-lang-single/1201573399",
     image: "https://m.media-amazon.com/images/I/91S-wf7UPML._SS500_.jpg",
@@ -50,7 +50,7 @@ var albums = [
   },
   {
     title: "Drive Down Sunset (feat. Loza)",
-    artist: "Samuelthe1st",
+    artist: "SamuelThe1st",
     url:
       "https://itunes.apple.com/us/album/drive-down-sunset-single/1201555541",
     image: "https://m.media-amazon.com/images/I/81CoyyBCKwL._SS500_.jpg",
