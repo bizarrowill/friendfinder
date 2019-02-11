40d1771f082c1e13091a89d3a3accd22cb793c97
Will R <williamp.robertson@gmail.com>
2019-02-10 17:53:44 -0700

    added url2 to album api for spotify

--- app/data/albums.js
+++ app/data/albums.js
@@ -3,6 +3,7 @@ var albums = [
     title: "SELF",
     artist: "SamuelThe1st",
     url: "https://itunes.apple.com/us/album/self/1446538476",
+    url2: "https://open.spotify.com/track/1P0M7iWSflGXPacpVZb10N",
     image: "https://m.media-amazon.com/images/I/711xF8AIF9L._SS500_.jpg",
     thumbnail_image:
       "https://instagram.fphx1-1.fna.fbcdn.net/vp/a30c15ffacdaeb94f8edb49e1a6b96a2/5CE84841/t51.2885-19/s150x150/50602409_1259502774227622_243420801963917312_n.jpg?_nc_ht=instagram.fphx1-1.fna.fbcdn.net"
@@ -11,6 +12,7 @@ var albums = [
     title: "Time's Up - Single",
     artist: "SamuelThe1st",
     url: "https://itunes.apple.com/us/album/times-up/1450002682?i=1450002685",
+    url2: "https://open.spotify.com/track/1P0M7iWSflGXPacpVZb10N",
     image: "https://m.media-amazon.com/images/I/71FruGreD5L._SS500_.jpg",
     thumbnail_image:
       "https://instagram.fphx1-1.fna.fbcdn.net/vp/a30c15ffacdaeb94f8edb49e1a6b96a2/5CE84841/t51.2885-19/s150x150/50602409_1259502774227622_243420801963917312_n.jpg?_nc_ht=instagram.fphx1-1.fna.fbcdn.net"
@@ -19,6 +21,7 @@ var albums = [
     title: "Yuh - EP",
     artist: "SamuelThe1st",
     url: "https://itunes.apple.com/us/album/yuh-ep/1293513201",
+    url2: "https://open.spotify.com/album/7tMDzsWMWwj7iNn1Y6IDjf",
     image: "https://m.media-amazon.com/images/I/71N6-aRpGaL._SS500_.jpg",
     thumbnail_image:
       "https://instagram.fphx1-1.fna.fbcdn.net/vp/a30c15ffacdaeb94f8edb49e1a6b96a2/5CE84841/t51.2885-19/s150x150/50602409_1259502774227622_243420801963917312_n.jpg?_nc_ht=instagram.fphx1-1.fna.fbcdn.net"
@@ -27,6 +30,7 @@ var albums = [
     title: "Bo Jackson - Single",
     artist: "SamuelThe1st",
     url: "https://itunes.apple.com/us/album/bo-jackson-single/1201539218",
+    url2: "https://open.spotify.com/track/5rVd38Tds4rW6HpIu8CaFG",
     image: "https://m.media-amazon.com/images/I/91ejgLZN-RL._SS500_.jpg",
     thumbnail_image:
       "https://instagram.fphx1-1.fna.fbcdn.net/vp/a30c15ffacdaeb94f8edb49e1a6b96a2/5CE84841/t51.2885-19/s150x150/50602409_1259502774227622_243420801963917312_n.jpg?_nc_ht=instagram.fphx1-1.fna.fbcdn.net"
@@ -35,6 +39,7 @@ var albums = [
     title: "Green Room - Single",
     artist: "SamuelThe1st",
     url: "https://itunes.apple.com/us/album/green-room/1201464122?i=1201465071",
+    url2: "https://open.spotify.com/track/26tDTmJlnvXQ01ExgXCFdO",
     image: "https://m.media-amazon.com/images/I/A1TcVqx0v5L._SS500_.jpg",
     thumbnail_image:
       "https://instagram.fphx1-1.fna.fbcdn.net/vp/a30c15ffacdaeb94f8edb49e1a6b96a2/5CE84841/t51.2885-19/s150x150/50602409_1259502774227622_243420801963917312_n.jpg?_nc_ht=instagram.fphx1-1.fna.fbcdn.net"
@@ -45,14 +50,18 @@ var albums = [
     url:
       "https://itunes.apple.com/us/album/limelit-feat-lang-single/1201573399",
     image: "https://m.media-amazon.com/images/I/91S-wf7UPML._SS500_.jpg",
+    url2: "hhttps://open.spotify.com/track/7nofH0SKrfiFCmp5xQov92",
+    image: "https://m.media-amazon.com/images/I/91S-wf7UPML._SS500_.jpg",
     thumbnail_image:
       "https://instagram.fphx1-1.fna.fbcdn.net/vp/a30c15ffacdaeb94f8edb49e1a6b96a2/5CE84841/t51.2885-19/s150x150/50602409_1259502774227622_243420801963917312_n.jpg?_nc_ht=instagram.fphx1-1.fna.fbcdn.net"
   },
+
   {
     title: "Drive Down Sunset (feat. Loza)",
     artist: "SamuelThe1st",
     url:
       "https://itunes.apple.com/us/album/drive-down-sunset-single/1201555541",
+    url2: "https://open.spotify.com/track/2mUPTIMQHuEgT36RdXu5JG",
     image: "https://m.media-amazon.com/images/I/81CoyyBCKwL._SS500_.jpg",
     thumbnail_image:
       "https://instagram.fphx1-1.fna.fbcdn.net/vp/a30c15ffacdaeb94f8edb49e1a6b96a2/5CE84841/t51.2885-19/s150x150/50602409_1259502774227622_243420801963917312_n.jpg?_nc_ht=instagram.fphx1-1.fna.fbcdn.net"
